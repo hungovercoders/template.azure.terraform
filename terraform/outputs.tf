427c056be17e86710ca3cf87595a113749b9a524
griff182uk <griff182uk@yahoo.co.uk>
2023-10-15 14:05:30 +0000

    added output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "resource_group_id" {
   value = azurerm_resource_group.rg.id
 }
+
+output "container_app_environment_id" {
+  value = azurerm_container_app_environment.container_environment.id
+}
