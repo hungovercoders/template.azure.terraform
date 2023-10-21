da7001448bc5fb7975c7f7f10c6b4a5de27d53e9
griff182uk <griff182uk@yahoo.co.uk>
2023-10-21 15:24:34 +0000

    removed bad variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "resource_group_id" {
   value = azurerm_resource_group.rg.id
 }
-
-output "container_app_environment_id" {
-  value = azurerm_container_app_environment.container_environment.id
-}
