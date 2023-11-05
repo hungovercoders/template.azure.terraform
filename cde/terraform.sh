559b54b97038f08be4247332f23ffadb014fa144
griff182uk <griff182uk@yahoo.co.uk>
2023-11-05 18:35:40 +0000

    Add export statements for Terraform variables

--- cde/terraform.sh
+++ cde/terraform.sh
@@ -1,12 +1,12 @@
 TF_BACKEND_RESOURCE_GROUP="state-rg-$UNIQUE_NAMESPACE"
 TF_BACKEND_STORAGE_ACCOUNT="statesa$UNIQUE_NAMESPACE"
 TF_BACKEND_CONTAINER=$ENVIRONMENT
-TF_VAR_environment=$ENVIRONMENT
-TF_VAR_unique_namespace=$UNIQUE_NAMESPACE
-TF_VAR_organisation=$ORGANISATION
-TF_VAR_region=$REGION
-TF_VAR_team=$TEAM
-TF_VAR_domain=$DOMAIN
+export TF_VAR_environment=$ENVIRONMENT
+export TF_VAR_unique_namespace=$UNIQUE_NAMESPACE
+export TF_VAR_organisation=$ORGANISATION
+export TF_VAR_region=$REGION
+export TF_VAR_team=$TEAM
+export TF_VAR_domain=$DOMAIN
 
 cd  terraform
 terraform init -backend-config="resource_group_name=$TF_BACKEND_RESOURCE_GROUP" -backend-config="storage_account_name=$TF_BACKEND_STORAGE_ACCOUNT" -backend-config="container_name=$TF_BACKEND_CONTAINER" -backend-config="key=template.azure.terraform.tfstate"
