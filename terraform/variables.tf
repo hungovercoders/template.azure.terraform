f5938e073a8fc66cf36a2eb25b0c44304c363b68
griff182uk <griff182uk@yahoo.co.uk>
2023-11-05 14:39:47 +0000

    Update resource group name with unique namespace in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -53,7 +53,7 @@ variable "unique_namespace" {
 locals {
   region_shortcode      = (var.region == "northeurope" ? "eun" : var.region == "westeurope" ? "euw" : "unk")
   environment_shortcode = (var.environment == "learning" ? "lrn" : var.environment == "development" ? "dev" : var.environment == "production" ? "prd" : "unk")
-  resource_group_name   = "${local.environment_shortcode}-${var.domain}01-rg"
+  resource_group_name   = "${local.environment_shortcode}-${var.domain}-rg-${var.unique_namespace}"
 
   tags = {
     environment  = var.environment
