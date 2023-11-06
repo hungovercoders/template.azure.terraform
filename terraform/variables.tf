28ddc1ec62ba17176829f5b5bf9b8ff44155dc6e
griff182uk <griff182uk@yahoo.co.uk>
2023-11-06 13:05:25 +0000

    Refactor terraform variables and update .gitpod.yml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -47,9 +47,9 @@ variable "unique_namespace" {
 }
 
 locals {
-  region_shortcode      = (var.region == "northeurope" ? "eun" : var.region == "westeurope" ? "euw" : "unk")
-  environment_shortcode = (var.environment == "learning" ? "lrn" : var.environment == "development" ? "dev" : var.environment == "production" ? "prd" : "unk")
-  resource_group_name   = "${local.environment_shortcode}-${var.domain}-rg-${var.unique_namespace}"
+  region_shortcode                  = (var.region == "northeurope" ? "eun" : var.region == "westeurope" ? "euw" : "unk")
+  environment_shortcode             = (var.environment == "learning" ? "lrn" : var.environment == "development" ? "dev" : var.environment == "production" ? "prd" : "unk")
+  resource_group_name               = "${local.environment_shortcode}-${var.domain}-rg-${var.unique_namespace}"
   storage_account_name              = "${local.environment_shortcode}${var.domain}sa${local.region_shortcode}${var.unique_namespace}"
   eventhub_namespace_name           = "${local.environment_shortcode}-${var.team}-ehns-${local.region_shortcode}-${var.unique_namespace}"
   databricks_workspace_name         = "${local.environment_shortcode}-${var.team}-dbw-${local.region_shortcode}-${var.unique_namespace}"
